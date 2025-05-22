ed3872835b32829d2cd1411819e5d57bfb7e74ee
Alejandroduoc <alej.rodriguezl@duocuc.cl>
2025-05-22 19:49:31 -0400

    port app

--- init.sql
+++ init.sql
@@ -19,5 +19,17 @@ CREATE TABLE IF NOT EXISTS ventas (
     cantidad INT NOT NULL,
     precio_venta DECIMAL(10, 2) NOT NULL,
     fecha_venta DATE NOT NULL,
-    fecha_actual TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    fecha_actual TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (codigo_producto) REFERENCES productos(codigo) -- Asumiendo que productos.codigo será UNIQUE y NOT NULL para ser FK
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS sucursal (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  codigo_sucursal VARCHAR(50) NOT NULL UNIQUE,
+  nombre VARCHAR(100) NOT NULL,
+  direccion TEXT DEFAULT NULL,
+  telefono VARCHAR(20) DEFAULT NULL,
+  creado_en TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+  actualizado_en TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
