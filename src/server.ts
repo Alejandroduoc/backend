600fd8185c800da79f4261b987145bff52ad5760
FranciscoDiazPinto <137012203+FranciscoDiazPinto@users.noreply.github.com>
2025-05-28 01:27:40 -0400

    listo swagger

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,10 @@
-import express from 'express';
-import productRoutes from './routes/product.routes';
-import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
+import app from './app';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
 
-// Configuración de Swagger
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API de Productos',
-      version: '1.0.0',
-      description: 'Documentación de la API para productos',
-    },
-  },
-  apis: ['./src/routes/*.ts'], // Ajusta la ruta si es necesario
-};
+const PORT = process.env.PORT || 3000;
 
-const swaggerSpec = swaggerJSDoc(swaggerOptions);
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use(productRoutes);
-
-export default app;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
